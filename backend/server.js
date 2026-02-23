b7d1cac809fd68b45195bed3fbeab73fec8d8d6d
manoranjan-sethi <manoranjansethi97@gmail.com>
2026-02-24 00:50:06 +0530

    fix: cors error fix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
+
+// allow frontend origin (required when frontend is on a different port)
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
